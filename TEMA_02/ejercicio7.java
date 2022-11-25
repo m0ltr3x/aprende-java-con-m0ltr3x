7b04f546dacd36a138ea4ff4820a8735fab152df
m0ltr3x <119032735+m0ltr3x@users.noreply.github.com>
2022-11-25 20:29:10 +0100

    Update ejercicio7.java

--- TEMA_02/ejercicio7.java
+++ TEMA_02/ejercicio7.java
@@ -2,8 +2,8 @@ package gitHub2;
 
 /**
  *                                              EJERCICIO 7 | TEMA_02 | VARIABLES 
- * Escribe un programa que declare 5 variables de tipo char. A continuación, crea otra variable como cadena de caracteres y asígnale como 
- * valor la concatenación de las anteriores 5 variables. Por último, muestra la cadena de caracteres por pantalla ¿Qué problemas te 
+ * Escribe un programa que declare 7 variables de tipo char. A continuación, crea otra variable como cadena de caracteres y asígnale como 
+ * valor la concatenación de las anteriores 7 variables. Por último, muestra la cadena de caracteres por pantalla ¿Qué problemas te 
  * encuentras? ¿cómo lo has solucionado?
  * 
  * @author M0ltr3X
