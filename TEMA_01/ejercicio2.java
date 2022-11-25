222c681376fca061aa8b3b04016fbcd5b99e8ad3
m0ltr3x <119032735+m0ltr3x@users.noreply.github.com>
2022-11-25 18:49:20 +0100

    Add files via upload

---
+++ TEMA_01/ejercicio2.java
@@ -0,0 +1,16 @@
+package github;
+
+/**
+ *          EJERCICIO 2 | TEMA_01 | SALIDA DE DATOS POR PANTALLA
+ * Modifica el programa anterior para que además se muestre tu dirección y tu número de
+ * teléfono. Asegúrate de que los datos se muestran en líneas separadas.
+ * 
+ * @author M0ltr3X
+ */
+public class ejercicio2 {
+    public static void main(String[] args) {
+        System.out.println("Nombre: " + "M0ltr3x");
+        System.out.println("Direccion: " + "C/Avenida Inventada, n8");
+        System.out.println("Telefono: "+"666-666-666");
+    }
+}
