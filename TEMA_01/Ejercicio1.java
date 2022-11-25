403570161d676784cf6500064c576a2901fa0706
m0ltr3x <119032735+m0ltr3x@users.noreply.github.com>
2022-11-25 18:48:48 +0100

    Add files via upload

---
+++ TEMA_01/Ejercicio1.java
@@ -0,0 +1,14 @@
+package github;
+
+/**
+ *    EJERCICIO 1 | TEMA_01 | SALIDA DE DATOS POR PANTALLA
+ *               MUESTRA POR PANTALLA TU NOMBRE
+ * 
+ * @author M0ltr3X
+ */
+public class Ejercicio1 { //CLASE PRINCIPAL LLAMADA EJERCICIO 1
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo soy M0ltr3x!!");
+    }
+    
+}
