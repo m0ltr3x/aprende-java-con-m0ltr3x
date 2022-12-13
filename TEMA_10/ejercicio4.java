9ca32bd7f0d7848410f1793b990a7fb79744d217
m0ltr3x <119032735+m0ltr3x@users.noreply.github.com>
2022-12-13 12:15:32 +0100

    Update ejercicio4.java

--- TEMA_10/ejercicio4.java
+++ TEMA_10/ejercicio4.java
@@ -3,8 +3,15 @@ package practica11;
 import java.util.Scanner;
 
 /**
+*  Realiza un simulador de máquina tragaperras simplificada que cumpla los siguientes
+*                   requisitos:
+* a) El ordenador mostrará una tirada que consiste en mostrar 3 figuras. Hay 5 figuras posibles: corazón, diamante, herradura, campana y limón.
+* b) Si las tres figuras son diferentes se debe mostrar el mensaje “Lo siento, ha perdido”.
+* c) Si hay dos figuras iguales y una diferente se debe mostrar el mensaje “Bien, ha recuperado su moneda”.
+* d) Si las tres figuras son iguales se debe mostrar “Enhorabuena, ha ganado 10 monedas”. 
  *
- * @author josebartor
+ *
+ * @author m0ltr3x
  */
 public class ejercicio4 {
 
