2dbae90ea5ac1253a009d0e8b0b0e3a649190b69
m0ltr3x <119032735+m0ltr3x@users.noreply.github.com>
2022-12-13 12:14:06 +0100

    Update ejercicio2.java

--- TEMA_10/ejercicio2.java
+++ TEMA_10/ejercicio2.java
@@ -4,7 +4,10 @@ import java.util.Scanner;
 
 /**
  *
- * @author josebartor
+ * Realiza un programa que llene la pantalla de caracteres aleatorios (a lo Matrix) con el código ascii entre el 32 y el 126. 
+ * Puedes hacer casting con (char) para convertir un entero en un carácter
+ *
+ * @author m0ltr3x
  */
 public class ejercicio2 {
     static Scanner keyboard = new Scanner(System.in);
